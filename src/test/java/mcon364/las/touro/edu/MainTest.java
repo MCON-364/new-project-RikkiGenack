be3561f06d214c389d37fe5d6be383ef95ce1d04
egoldfar <egoldfar@student.touro.edu>
2026-02-10 00:38:58 -0500

    added unit test

--- src/test/java/mcon364/las/touro/edu/MainTest.java
+++ src/test/java/mcon364/las/touro/edu/MainTest.java
@@ -10,6 +10,18 @@ public class MainTest {
     void testGetUserName(){
         assertFalse(Main.getUserName("USERNAME").isEmpty());
         assertEquals(System.getenv("USERNAME") , Main.getUserName("USERNAME").get());
+        assertTrue(Main.getUserName("NULL").isEmpty());
     }
 
+
+    @Test
+    void testGetGreeting(){
+        assertEquals("Hello, World", Main.getGreeting("Null"));
+        assertEquals("Hello, " + System.getenv("USERNAME"), Main.getGreeting("USERNAME"));
+    }
+
+    @Test
+    void testProcessValues() {
+
+    }
 }
